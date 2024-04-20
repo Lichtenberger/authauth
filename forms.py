0fa032ad27750825ff70e110b5de0a29aea46c68
Michael Lichtenberger <mikelichtenberger81@gmail.com>
2024-04-20 12:50:38 -0500

    auth

---
+++ forms.py
@@ -0,0 +1,28 @@
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Length, NumberRange, Email, Optional
+from flask_wtf import FlaskForm
+
+class LoginForm(FlaskForm):
+
+    username = StringField('Username', validators=[InputRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=25)])
+
+
+class RegistrationFrom(FlaskForm):
+
+    username = StringField('Username', validators=[InputRequired(), Length(min=2, max=20)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=25)])
+    email = StringField('Email', validators=[InputRequired(), Email(), Length(max=50)])
+    first_name = StringField('First Name', validators=[InputRequired(), Length(max=30)])
+    last_name = StringField('Lsst Name', validators=[InputRequired(), Length(max=30)])
+
+
+class FeedbackForm(FlaskForm):
+
+    title = StringField('Title', validators=[InputRequired(), Length(max=100)])
+    content = StringField('Content', validators=[InputRequired()])
+
+
+class DeleteForm(FlaskForm):
+
+    
